9a17a56d7c6eb0090b92b7ece151dfa452ab767f
fasil-advance <fasil@advanceinfotech.io>
2022-04-21 13:18:46 +0530

    template

---
+++ public/template/assets/js/script.js
@@ -0,0 +1,16 @@
+$('#clock-c').countdown('2022/06/01', function(event) {
+    var $this = $(this).html(event.strftime('' +
+        '<span class="counter-text">%D :<span class="counter-line"> Days</span> </span>' +
+        '<span class="counter-text">%H :<span class="counter-line"> Hours</span> </span>' +
+        '<span class="counter-text">%M :<span class="counter-line"> Minutes</span> </span>' +
+        '<span class="counter-text">%S<span class="counter-line">Seconds</span></span>'));
+});
+
+$(document).ready(function() {
+    $('#side_opener').click(function() {
+        $("#navbar-menu").fadeIn(500)
+    });
+    $('#side_closer').click(function() {
+        $("#navbar-menu").fadeOut(500)
+    });
+});
