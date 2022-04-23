83e813f8d2324223457ab7e90e97d83fa391ba93
fasil-advance <fasil@advanceinfotech.io>
2022-04-23 12:31:12 +0530

    template

--- public/template/assets/js/script.js
+++ public/template/assets/js/script.js
@@ -1,9 +1,12 @@
 $('#clock-c').countdown('2022/06/01', function(event) {
-    var $this = $(this).html(event.strftime('' +
-        '<span class="counter-text">%D :<span class="counter-line"> Days</span> </span>' +
-        '<span class="counter-text">%H :<span class="counter-line"> Hours</span> </span>' +
-        '<span class="counter-text">%M :<span class="counter-line"> Minutes</span> </span>' +
-        '<span class="counter-text">%S<span class="counter-line">Seconds</span></span>'));
+    var $this = $(this).html(event.strftime(
+        '<span class="counter-text">' +
+            '<span class="number">%D <span class="counter-line">Days</span></span>' + '<span class="seperator"> : </span>' +
+            '<span class="number">%H <span class="counter-line">Hours</span></span>' + '<span class="seperator"> : </span>' +
+            '<span class="number">%M <span class="counter-line">Minutes</span></span>' + '<span class="seperator"> : </span>' +
+            '<span class="number">%S <span class="counter-line">Seconds</span></span>' +
+        '</span>'
+    ));
 });
 
 $(document).ready(function() {
