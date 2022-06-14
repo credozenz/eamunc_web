8532653bb0a3191fc2503a801b9c065eec5d3627
ajil-advance <ajil@advanceinfotech.io>
2022-06-14 15:51:20 +0530

    style fix

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -8,7 +8,7 @@
     <img class="mySlides" src="{{ asset('uploads/'.$value->image) }}">
     @endforeach
 
-    <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+    <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%;top: 0;margin: auto;height: 2rem;">
     @foreach ($banner as $key => $value)
     <span class="demo "></span>
     @endforeach
