d8f7a25112270a635e71cf5e2743310a0877b6f3
ajil-advance <ajil@advanceinfotech.io>
2022-06-27 10:49:03 +0530

    bug fix

--- resources/views/web/isg-registration.blade.php
+++ resources/views/web/isg-registration.blade.php
@@ -88,16 +88,16 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="form-label">MUN Experience (if any) *</label>
-                                        <input type="text" name="mun_experience" value="{{ old('mun_experience') }}" maxlength="80" class="form-control @error('mun_experience') border-danger @enderror" {{ $errors->has('mun_experience') ? 'autofocus' : '' }} placeholder="MUN Experience (if any)" aria-describedby="textHelp" required>
+                                        <label for="form-label">MUN Experience (if any)</label>
+                                        <input type="text" name="mun_experience" value="{{ old('mun_experience') }}" maxlength="80" class="form-control @error('mun_experience') border-danger @enderror" {{ $errors->has('mun_experience') ? 'autofocus' : '' }} placeholder="MUN Experience (if any)" aria-describedby="textHelp" >
                                         @error('mun_experience')<div class="text-danger mt-2">{{ $message }}</div>@enderror
                                     </div>
                                 </div>
 
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="form-label">Bureau Member Experience*</label>
-                                        <input type="text" name="bureaumem_experience" value="{{ old('bureaumem_experience') }}" maxlength="80" class="form-control @error('bureaumem_experience') border-danger @enderror" {{ $errors->has('bureaumem_experience') ? 'autofocus' : '' }} placeholder="Bureau Member Experience" aria-describedby="textHelp" required>
+                                        <label for="form-label">Bureau Member Experience</label>
+                                        <input type="text" name="bureaumem_experience" value="{{ old('bureaumem_experience') }}" maxlength="80" class="form-control @error('bureaumem_experience') border-danger @enderror" {{ $errors->has('bureaumem_experience') ? 'autofocus' : '' }} placeholder="Bureau Member Experience" aria-describedby="textHelp" >
                                         @error('bureaumem_experience')<div class="text-danger mt-2">{{ $message }}</div>@enderror
                                     </div>
                                 </div>
