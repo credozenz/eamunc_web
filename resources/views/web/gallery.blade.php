7843ae40bee5500e8dfacce0db24d9dd3d11740c
ajil-advance <ajil@advanceinfotech.io>
2022-10-26 10:23:45 +0530

    Update gallery.blade.php

--- resources/views/web/gallery.blade.php
+++ resources/views/web/gallery.blade.php
@@ -1,5 +1,6 @@
 @extends('web.layout.main')
 @section('content')
+
 <section id="hero" class="section-padding">
         <div class="wrapper">
             <div class="container">
@@ -9,23 +10,27 @@
                     </div>
 
                     <div class="row mt-5">
+
                     @if (!empty($gallery) && $gallery->count())
                         @foreach ($gallery as $key => $value)
-                        <a href="{{ route('gallery-inner',$value->id ?? '') }}" class="color-white">
+                       
                         <div class="col-lg-3 col-md-4 col-6 image-box mb-4 mb-5">
-                        <img src="{{ asset('uploads/'.$value->cover_image) ?? '' }}" alt="{{ $value->name ?? '' }}">
+                            <a href="{{ route('gallery-inner',$value->id ?? '') }}" class="color-white">
+                            <img src="{{ asset('uploads/'.$value->cover_image) ?? '' }}" alt="{{ $value->name ?? '' }}">
                             <p class="mentor-name text-start">{{ $value->name ?? '' }}</p>
+                            </a>
                         </div>
-                        </a>
+                        
                         @endforeach
                     @else
                     <div class="col-md-6 text-center text-md-end color-darkblue">
                        Gallerys isn't available right Now !
                     </div>
                     @endif
+                      
+
 
                     </div>
-
                 </div>
 
                 <span class="spacer"></span>
@@ -34,5 +39,6 @@
         </div>
     </section>
 
+    
 
     @endsection
