35139814dfa52b02f7114523a55a1af3b9e7ae92
ajil-advance <ajil@advanceinfotech.io>
2023-04-07 16:39:15 +0530

    Create VIPUserChecker.php

---
+++ app/Http/Middleware/App/VIPUserChecker.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware\App;
+use Session;
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\User;
+use View;
+
+class VIPUserChecker
+{
+  
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+       
+        if(Session::has('Log_ID'))
+        {
+          
+           
+
+            if(Session::get('Log_ROLE') == '4')
+            {
+                $log_member = Session::get('Log_ID');
+                
+                $member = User::where('users.deleted_at', null)
+                ->select('users.*')
+                ->where('users.id', '=' , $log_member)
+                ->first();
+                View::share('member', $member);
+
+                return $next($request); 
+            }  else  {
+                return redirect('/app');          
+            }
+
+            
+        }
+        else
+        {
+            return redirect('/app');          
+        }
+ 
+      
+    }
+}
