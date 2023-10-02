18b0c302989e5a396df0b9adffed4ad3522f4b62
ajil-advance <83936627+ajil-advance@users.noreply.github.com>
2023-10-02 12:07:32 +0400

    Update ForgotPasswordController.php
    ForgetPassword validation check user active or not

--- app/Http/Controllers/Admin/ForgotPasswordController.php
+++ app/Http/Controllers/Admin/ForgotPasswordController.php
@@ -15,6 +15,7 @@ use App\Models\Students;
 use Mail;
 use Illuminate\Support\Str;
 use View;
+use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
 {
@@ -43,12 +44,37 @@ class ForgotPasswordController extends Controller
     }
 
     public function ForgetPasswordStore(Request $request) {
-        
-        $request->validate([
-            'email' => 'required|email|exists:users',
-        ], [
-            'email.exists' => 'The selected email does not exist in our records.',
-        ]);
+
+         // Define custom validation messages
+    $customMessages = [
+        'email.exists' => 'The selected email does not exist in our records.',
+    ];
+
+    // Define custom validation rules with additional conditions
+    $validator = Validator::make($request->all(), [
+        'email' => [
+            'required',
+            'email',
+            function ($attribute, $value, $fail) {
+                $user = DB::table('users')
+                    ->where('email', $value)
+                    ->whereNull('deleted_at')
+                    ->where('status', 1)
+                    ->first();
+
+                if (!$user) {
+                    $fail('The selected email (' . $value . ') does not exist in our records or the account is not active.');
+                }
+            },
+        ],
+    ], $customMessages);
+
+    // Check if validation fails
+    if ($validator->fails()) {
+        return redirect()->back()
+            ->withErrors($validator)
+            ->withInput();
+    }
 
         $token = Str::random(64);
         DB::table('password_resets')->insert([
